4bd556cfdfa09b63a205198a66888655a4a8a91d
sixStone <zhaoleilei2015@163.com>
2016-11-02 09:28:12 +0800

    Create regexp.js

---
+++ regexp.js
@@ -0,0 +1,16 @@
+//此文件记录一些常见功能的正则
+"use strict";
+
+//将url参数中具有指定参数名的参数值返回;
+function GetQueryString(name){
+    var reg = new RegExp("(^|&)"+ name +"=([^&]*)(&|$)");
+    var r = window.location.search.substr(1).match(reg);
+    if(r != null){
+        return  decodeURI(r[2]);
+    }
+    return null;
+}
+
+// 将数字转换为国际货币形式(三位一个逗号,)
+var reg = /(\d{1,3})(?=(?:\d{3})+(?!\d))/g;
+str = str.toString().replace(reg,'$1,');
