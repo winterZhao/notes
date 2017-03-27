77971ccb7e531d897088c31b5057ca50cd5043b3
sixStone <zhaoleilei2015@163.com>
2017-03-27 11:41:58 +0800

    Update regexp.js

--- regexp.js
+++ regexp.js
@@ -11,6 +11,16 @@ function GetQueryString(name){
     return null;
 }
 
+function GetQueryString(name){
+        var reg = new RegExp("(^|&)"+ name +"=([^&]*)(&|$)");
+        var r = window.location.search.substr(1).match(reg);
+        if(r != null){
+            var a = decodeURI(r[2]);
+
+            return a.replace(/\&/g, "&amp;").replace(/\</g, "&lt;").replace(/\>/g, "&gt;").replace(/"/g, "&quot;").replace(/'/g, "&apos;");
+        }
+        return null;
+    }
 // 将数字转换为国际货币形式(三位一个逗号,)
 var reg = /(\d{1,3})(?=(?:\d{3})+(?!\d))/g;
 str = str.toString().replace(reg,'$1,');
